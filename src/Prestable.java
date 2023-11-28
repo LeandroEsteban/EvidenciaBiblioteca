7b2c4965736f5363b7b5807eada7b1cda414d2e3
Leandro Matamoros <127903058+LeandroEsteban@users.noreply.github.com>
2023-11-27 22:11:35 -0300

    Delete src directory

--- src/Prestable.java
+++
@@ -1,4 +0,0 @@
-public interface Prestable {
-    public void prestar();
-    public void devolver();
-}
--- src/Prestamo.java
+++
@@ -1,8 +0,0 @@
-public class Prestamo {
-    private int idUsuario;
-    private String nombreUsuario;
-    private String emailUsuario;
-    private String Titulo;
-    private String Autor;
-
-}
